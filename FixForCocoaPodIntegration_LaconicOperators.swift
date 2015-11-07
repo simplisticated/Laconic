3fa3389131d773e8514a3109213abf4524423d5e
Igor Matyushkin <igormatyushkin014@gmail.com>
2015-11-07 16:17:20 +0300

    Added fix for CocoaPod integration

---
+++ FixForCocoaPodIntegration_LaconicOperators.swift
@@ -0,0 +1,38 @@
+//
+//  FixForCocoaPodIntegration_LaconicOperators.swift
+//  Laconic
+//
+//  Created by Igor Matyushkin on 07.11.15.
+//  Copyright © 2015 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+prefix operator √ { }
+
+prefix operator √√ { }
+
+prefix operator √√√ { }
+
+prefix operator √√√√ { }
+
+prefix operator √√√√√ { }
+
+prefix operator √√√√√√ { }
+
+prefix operator √√√√√√√ { }
+
+prefix operator √√√√√√√√ { }
+
+prefix operator √√√√√√√√√ { }
+
+prefix operator √√√√√√√√√√ { }
+
+infix operator ≤ { }
+
+infix operator ≥ { }
+
+prefix operator ∑ { }
+
+infix operator ~> { associativity left }
+
